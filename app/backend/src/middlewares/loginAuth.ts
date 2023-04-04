36b960fa965bb304b4a895a097b14b10af15eccf
Rodrigo Neves <rnevesdemello@gmail.com>
2023-04-04 18:45:32 -0300

    feat: desenvolve o endpoint login de modo a não permitir acesso com email não cadastrado ou senha incorreta

--- app/backend/src/middlewares/loginAuth.ts
+++ app/backend/src/middlewares/loginAuth.ts
@@ -1,11 +1,40 @@
 import { Request, Response, NextFunction } from 'express';
+import * as bcrypt from 'bcryptjs';
+import Users from '../database/models/Users';
 
-const loginAuth = (req: Request, res: Response, next: NextFunction) => {
+const emailRegex = /^[\w+\-.]+@[a-z\d-]+(\.[a-z]+)*\.[a-z]+$/;
+const errorMessage = 'Invalid email or password';
+
+const validateInput = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
+
   if (!email || !password) {
     return res.status(400).json({ message: 'All fields must be filled' });
   }
   next();
 };
 
-export default loginAuth;
+const validateUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  const users = await Users.findOne({ where: { email } });
+  if (!users) {
+    return res.status(401).json({ message: errorMessage });
+  }
+
+  if (!emailRegex.test(email)) {
+    return res.status(401).json({ message: errorMessage });
+  }
+
+  const pass = await bcrypt.compare(password, users.password);
+  if (!pass) {
+    return res.status(401).json({ message: errorMessage });
+  }
+  if (password.length < 6) {
+    return res.status(401).json({ message: errorMessage });
+  }
+
+  next();
+};
+
+export { validateInput, validateUser };
--- app/backend/src/routes/users.routers.ts
+++ app/backend/src/routes/users.routers.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import UserControler from '../controllers/users.controllers';
-import loginAuth from '../middlewares/loginAuth';
+import { validateInput, validateUser } from '../middlewares/loginAuth';
 
 const teamsRouter = Router();
 
-teamsRouter.post('/', loginAuth, UserControler.login);
+teamsRouter.post('/', validateInput, validateUser, UserControler.login);
 
 export default teamsRouter;
