3667d8e4bdffb0866ec594f728d4b0b1a72a54ad
Rodrigo Neves <rnevesdemello@gmail.com>
2023-04-07 18:29:41 -0300

    feat: cria model e migration para matches

--- app/backend/src/database/models/Matches.ts
+++ app/backend/src/database/models/Matches.ts
@@ -1,37 +1,67 @@
-// import { Model, INTEGER, STRING } from 'sequelize';
-// import db from '.';
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './Teams';
 
-// class Matches extends Model {
-//   declare id: number;
-// }
+class Matches extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
 
-// Matches.init(
-//   {
-//     id: {
-//       type: INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     username: {
-//       type: STRING,
-//     },
-//     role: {
-//       type: STRING,
-//     },
-//     email: {
-//       type: STRING,
-//     },
-//     password: {
-//       type: STRING,
-//     },
-//   },
-//   {
-//     underscored: true,
-//     sequelize: db,
-//     timestamps: false,
-//     modelName: 'matches',
-//   },
-// );
+Matches.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    homeTeamId: {
+      type: INTEGER,
+      allowNull: false,
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      references: {
+        model: 'teams',
+        key: 'id',
+      },
+    },
+    homeTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeamId: {
+      type: INTEGER,
+      allowNull: false,
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      references: {
+        model: 'teams',
+        key: 'id',
+      },
+    },
+    awayTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    inProgress: {
+      type: BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    timestamps: false,
+    modelName: 'matches',
+    tableName: 'matches',
+  },
+);
 
-// export default Matches;
+Matches.belongsTo(Teams, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+
+export default Matches;
